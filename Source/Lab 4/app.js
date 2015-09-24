e60391966f2c62a7471abbd6fbd8819a766d538d
Harshin1 <HM374@KC-SCE460LAB-10.kc.umkc.edu>
2015-09-24 11:09:38 -0500

    Lab 4

---
+++ Source/Lab 4/app.js
@@ -0,0 +1,42 @@
+var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+
+animateApp.config(function($routeProvider) {
+    $routeProvider
+    	.when('/', {
+    		templateUrl: 'home.html',
+            controller: 'mainController'
+    	})
+    	.when('/about', {
+    		templateUrl: 'about.html',
+            controller: 'aboutController'
+    	})
+    	.when('/contact', {
+    		templateUrl: 'contact.html',
+            controller: 'contactController'
+    	});
+
+});
+
+animateApp.controller('mainController', function($scope) {
+    $scope.pageClass = 'home';
+});
+
+animateApp.controller('aboutController', function($scope) {
+    $scope.pageClass = 'about';
+});
+
+animateApp.controller('contactController', function($scope) {
+    $scope.pageClass = 'contact';
+});
+
+function LoginController($scope) {
+    
+    $scope.logins = [];
+    $scope.login = function (user, pwd) {
+        localStorage.setItem("name" , user);
+        $scope.logins.push( localStorage.getItem("name") + " was logged in.");
+        window.location = "about.html";
+       
+    };
+    
+}
