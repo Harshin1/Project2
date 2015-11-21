206deac490d35449b28a52a3f79e3d72ef00992d
Harshin1 <hm374@mail.umkc.edu>
2015-11-20 22:58:48 -0600

    Lab 10

---
+++ Source/Lab 10/src/com/ibm/cloudoe/samples/HelloResource.java
@@ -0,0 +1,18 @@
+package com.ibm.cloudoe.samples;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+//@Path("/hello")
+public class HelloResource {
+
+	//@GET
+	public String getInformation() {
+
+	
+		
+		return "Hi World!";
+
+	}
+}
